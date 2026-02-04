4b810050e7c829636cf7ba1788b026ad920f45bc
elfaoz <nashers.manziel@gmail.com>
2026-02-04 23:39:25 +0700

    Fix: use IP server and correct directory

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3baq7v63ebg"
+    "revision": "0.343e203g69g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
